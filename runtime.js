7ae51e92b45b2ebed1bd4a6b541d2168c343efef
tnn1nja <thenonsenseninja@gmail.com>
2024-07-09 01:11:53 +0100

    Randomiser Restyled

--- runtime.js
+++ runtime.js
@@ -1,15 +1,36 @@
-const focused = [];
-function selectRandom(){
-    clearSelection();
-    const rand = Math.floor(Math.random() * (74)) + 1
-    const chosen = document.getElementById("tr" + rand)
-    focused.push(chosen);
-    chosen.classList.add("focus")
-    chosen.scrollIntoView({ behavior: 'smooth', block: 'center' });
+var focused = null;
+onButton = false;
+
+document.addEventListener('mousedown', click);
+
+function toggleOnButton(){
+    if(onButton){
+        onButton = false;
+    }else{
+        onButton = true;
+    }
 }
 
-function clearSelection(){
-    for(element of focused){
-        element.classList.remove("focus");
+function click(){
+    if(!onButton){
+        clear();
     }
+}
+
+function random(){
+    if(focused == null){
+        const rand = Math.floor(Math.random() * (getMaxTr())) + 1
+        focused = document.getElementById("tr" + rand)
+        focused.classList.add("focus")
+        focused.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        document.getElementById("bimg").src = "../resources/buttons/cancel.png";
+    }else{
+        clear();
+    }
+}
+
+function clear(){
+    focused.classList.remove("focus");
+    focused = null;
+    document.getElementById("bimg").src = "../resources/buttons/random.png";
 }
